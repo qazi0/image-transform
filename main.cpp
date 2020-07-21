a51fe48ac3beabb8fdfc97ece8e9860737856cc5
Siraj Qazi <fullbridgerectifier@gmail.com>
2020-07-21 10:58:17 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-/**
- * @file main.cpp
- * A simple C++ program that manipulates an image.
- *
- * @author University of Illinois CS 225 Course Staff
- * @author Updated by University of Illinois CS 400 Course Staff
-**/
 
+// Main driver file
 #include "ImageTransform.h"
 
 int main() {

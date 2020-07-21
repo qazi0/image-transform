b49b017456a9d71bd48e1f9f7ca865c6b4d1b4c6
Siraj Qazi <fullbridgerectifier@gmail.com>
2020-07-21 10:22:24 +0500

    updated HSLA class

--- uiuc/HSLAPixel.h
+++ uiuc/HSLAPixel.h
@@ -1,10 +1,3 @@
-/**
- * @file HSLAPixel.h
- *
- * @author University of Illinois CS 225 Course Staff
- * @version 2018r1-lab1 - Updated for CS 400
- */
-
 #pragma once
 
 #include <iostream>
@@ -12,8 +5,6 @@
 
 namespace uiuc {
 
-  // Put your HSLAPixel class definition here.
-  // (Remember to end it with a semicolon!)
   class HSLAPixel {
   public:
       HSLAPixel();

d9d99bdd42f756304453d35c91eb2af5d69d6c18
Siraj Qazi <siraj.qazi@seeloz.com>
2022-11-28 21:48:58 +0500

    added command line arguments api

--- main.cpp
+++ main.cpp
@@ -2,23 +2,54 @@
 // Main driver file
 #include "ImageTransform.h"
 
-int main() {
+int main(int argc, char **argv) {
     std::ios_base::sync_with_stdio(false);
-    uiuc::PNG png, png2, result;
 
-    png.readFromFile("alma.png");
-    result = grayscale(png);
-    result.writeToFile("out-grayscale.png");
+    if (argc < 4) {
+        std::cout << "Usage:\n"
+                     "\t" << argv[0] << " <mode> <src_image_path> <dst_image_path> [mask_img_path]\n"
+                                        "Where mode can be 'grayscale', 'spotlight', 'illinify' or 'watermark'\n";
+        exit(-1);
+    }
 
-    result = createSpotlight(png, 450, 150);
-    result.writeToFile("out-spotlight.png");
+    std::string mode(argv[1]);
+    std::string source_image_path(argv[2]);
+    std::string dest_image_path(argv[3]);
+    std::string mask_image_path; // [[maybe_unused]]
 
-    result = illinify(png);
-    result.writeToFile("out-illinify.png");
+    if (argc > 4)
+        mask_image_path = argv[4];
 
-    png2.readFromFile("overlay.png");
-    result = watermark(png, png2);
-    result.writeToFile("out-watermark.png");
+
+    uiuc::PNG src_img, dst_img, mask_img, result;
+
+    src_img.readFromFile(source_image_path);
+    if (mode == "grayscale") {
+        result = grayscale(src_img);
+        result.writeToFile(dest_image_path);
+    } else if (mode == "spotlight") {
+        result = createSpotlight(src_img, 450, 150);
+        result.writeToFile(dest_image_path);
+    } else if (mode == "illinify") {
+        result = illinify(src_img);
+        result.writeToFile(dest_image_path);
+    } else if (mode == "watermark") {
+        if (mask_image_path.empty()) {
+            std::cout << "Usage in 'watermark' mode requires [mask_img] path.\n\n"
+                         "\t" << argv[0] << " watermark <src_image_path> <dst_image_path> <mask_img_path>\n";
+            exit(-1);
+        }
+
+        mask_img.readFromFile(mask_image_path);
+        result = watermark(src_img, mask_img);
+        result.writeToFile(dest_image_path);
+    } else {
+        std::cout << "Invalid mode '" << mode << "' specified. Valid modes are 'grayscale', 'spotlight', "
+                                                 "'illinify' or 'watermark'.\n";
+        exit(-1);
+    }
+
+    std::cout << "[INFO] " << mode << "'d image successfully written to " << dest_image_path << "\n";
 
     return 0;
 }
